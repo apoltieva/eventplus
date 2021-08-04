6c82de5fcf4233403b52536cfac25d92719973ab
Anna Poltieva <apolti@softserveinc.com>
2021-08-04 14:45:08 +0300

    Finish Event model testing

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -4,11 +4,11 @@ require 'faker'
 
 FactoryBot.define do
   factory :event do
-    title { Faker::String.random(length: 3..12) }
+    title { Faker::Movie.title }
     description { Faker::Lorem.paragraph }
     artist { Faker::Name.name }
-    start_time { Faker::Time.between(from: DateTime.now, to: DateTime.now + 1) }
-    end_time { Faker::Time.between(from: DateTime.now + 2, to: DateTime.now + 3) }
-    venue_id { 1 }
+    start_time { Faker::Time.between(from: DateTime.new(2001,2,3,4,5,6), to: DateTime.new(2021,2,3,4,5,6)) }
+    end_time { Faker::Time.between(from: DateTime.new(2022,2,3,4,5,6), to: DateTime.new(2023,2,3,4,5,6)) }
+    association :venue
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,6 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
+  context "with valid attributes" do
+    it "should be valid" do
+      expect(create(:event)).to be_valid
+    end
+  end
   context 'with invalid attributes' do
     it 'is not valid without a title' do
       expect(build(:event, title: nil)).to_not be_valid
