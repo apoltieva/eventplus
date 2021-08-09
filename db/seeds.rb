4ff7a20165c4ec503e13d8e130d4ba6a2409355e
Anna Poltieva <apolti@softserveinc.com>
2021-08-09 19:22:06 +0300

    Seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,23 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first)\
+#
+if Venue.none?
+  10.times do
+    Venue.create!(name: Faker::Mountain.name,
+                  latitude: rand(50..51),
+                  longitude: rand(30..31))
+  end
+end
+
+if Event.none?
+  10.times do
+    Event.create!(title: Faker::Movie.title,
+                  description: Faker::Lorem.paragraph,
+                  artist: Faker::Name.name,
+                  start_time: Faker::Time.between(from: DateTime.new(2021, 9, 3, 4, 5, 6), to: DateTime.new(2021, 12, 3, 4, 5, 6)),
+                  end_time: Faker::Time.between(from: DateTime.new(2022, 2, 3, 4, 5, 6), to: DateTime.new(2023, 2, 3, 4, 5, 6)),
+                  venue_id: Venue.all.sample.id)
+  end
+end
--- spec/factories/venues.rb
+++ spec/factories/venues.rb
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
 # frozen_string_literal: true
 
-=======
->>>>>>> venue
 require 'faker'
 
 FactoryBot.define do
   factory :venue do
-<<<<<<< HEAD
-  end
-end
-=======
     name { Faker::Mountain.name }
     latitude { Faker::Address.latitude }
     longitude { Faker::Address.longitude }
   end
 end
->>>>>>> venue
