5e68a8bb1aa7af488cd336ba2a1216f883b63fd6
Anna Poltieva <apolti@softserveinc.com>
2021-08-09 14:31:33 +0300

    Finish registration

--- app/controllers/users_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-
-  def create; end
-end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,8 +3,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'eventplus.mailer@gmail.com'
   layout 'mailer'
-
-  def welcome
-    mail(to: 'poltyeva.anna@gmail.com', subject: 'Welcome to my App')
-  end
 end
