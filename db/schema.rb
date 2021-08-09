8f03cc11572fdae2dd57930bf35d0bf838af6bf0
Anna Poltieva <apolti@softserveinc.com>
2021-08-09 19:06:44 +0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20_210_803_134_457) do
-=======
 ActiveRecord::Schema.define(version: 2021_08_04_122649) do
 
->>>>>>> venue
-  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
   create_table 'active_storage_attachments', force: :cascade do |t|
     t.string 'name', null: false
     t.string 'record_type', null: false
@@ -31,16 +25,6 @@ ActiveRecord::Schema.define(version: 2021_08_04_122649) do
     t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
     t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness',
                                                     unique: true
-=======
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
->>>>>>> venue
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
@@ -55,17 +39,10 @@ ActiveRecord::Schema.define(version: 2021_08_04_122649) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-<<<<<<< HEAD
   create_table 'active_storage_variant_records', force: :cascade do |t|
     t.bigint 'blob_id', null: false
     t.string 'variation_digest', null: false
     t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
-=======
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
->>>>>>> venue
   end
 
   create_table "events", force: :cascade do |t|
