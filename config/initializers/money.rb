0c5a2652eedcb172edd69de1871779055c7b6ba5
Anna Poltieva <apolti@softserveinc.com>
2021-08-12 11:06:02 +0300

    Fix Money deprecation warnings

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -4,5 +4,7 @@ MoneyRails.configure do |config|
 
   # To set the default currency
   config.default_currency = :uah
-
+  I18n.locale = :en
+  Money.locale_backend = :i18n
+  Money.rounding_mode = BigDecimal::ROUND_HALF_UP
 end
