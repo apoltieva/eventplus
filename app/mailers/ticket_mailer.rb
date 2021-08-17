cfdb4cf77d44fb6997c3215151108ff69245efd0
Anna Poltieva <apolti@softserveinc.com>
2021-08-17 15:12:48 +0300

    Fix qr-code

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -1,21 +1,13 @@
 # frozen_string_literal: true
 
-# require_relative '../services/qr_code'
 class TicketMailer < ApplicationMailer
-  after_action :delete_qr_code
   def mail_tickets
     @user = params[:user]
     @event = params[:event]
     @uuid = params[:uuid]
     @qr = QrCode.generate("#{orders_url}/#{params[:uuid]}")
-    attachments.inline['image.png'] = File.read(@qr)
+    attachments.inline['qr.png'] = File.read(@qr)
     mail(to: @user.email, subject: 'Your Event+ tickets')
   end
 
-  private
-
-  def delete_qr_code
-    @qr = QrCode.generate("#{orders_url}/#{params[:uuid]}")
-    File.delete(@qr) if File.exist? @qr
-  end
 end
--- app/services/qr_code.rb
+++ app/services/qr_code.rb
@@ -2,10 +2,7 @@
 
 class QrCode
   def self.generate(url)
-    file_path = "/tmp/ticket-#{url}-qrcode.png"
-    unless File.exist? file_path
-      IO.binwrite(file_path, RQRCode::QRCode.new(url).as_png(size: 400).to_s)
-    end
-    file_path
+    IO.binwrite("/tmp/ticket-qrcode.png", RQRCode::QRCode.new(url).as_png(size: 400).to_s)
+    "/tmp/ticket-qrcode.png"
   end
 end
