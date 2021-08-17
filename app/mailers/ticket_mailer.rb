397c98243a09bade40e2a0f9f07483c3ebdbd33f
Anna Poltieva <apolti@softserveinc.com>
2021-08-17 19:05:21 +0300

    Fix pictures in ticket mailer

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -7,6 +7,9 @@ class TicketMailer < ApplicationMailer
     @uuid = params[:uuid]
     @qr = QrCode.generate("#{orders_url}/#{params[:uuid]}")
     attachments.inline['qr.png'] = File.read(@qr)
+    @event.pictures.each_with_index do |picture, i|
+      attachments["img_#{i}.png"] = picture.blob.download
+    end
     mail(to: @user.email, subject: 'Your Event+ tickets')
   end
 end
