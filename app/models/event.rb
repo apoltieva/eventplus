3c9926876e9a4cd3d69c65df6eda36ad4a6b9e2d
Anna Poltieva <apolti@softserveinc.com>
2021-09-13 09:58:16 +0300

    Fix event ordering

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Event < ApplicationRecord
+  extend OrderAsSpecified
+
   validates_presence_of :title, :start_time, :end_time
   monetize :ticket_price_cents, numericality: { greater_than_or_equal_to: 0 }
   validates :total_number_of_tickets, numericality: { greater_than_or_equal_to: 1 }
@@ -14,17 +16,8 @@ class Event < ApplicationRecord
   scope :past, -> { where('end_time <= ?', Time.now) }
   scope :future, -> { where('end_time > ?', Time.now) }
   scope :by_user, ->(id) { merge(User.find(id).events) if id }
-  def self.order_by_ids(ids)
-    t = Event.arel_table
-    condition = Arel::Nodes::Case.new(t[:id])
-    ids.each_with_index do |id, index|
-      condition.when(id).then(index)
-    end
-    order(condition)
-  end
   scope :nearest, ->(location) do
-    venue_ids = Venue.near(location, 20_000, units: :km).map(&:id)
-    where(venue_id: venue_ids).order_by_ids(venue_ids)
+    order_as_specified venue_id: Venue.near(location, 20_000, units: :km).map(&:id)
   end
   scope :filter_by, ->(filter, location, user_id) do
     case filter
