dd836187bdbd3e76ebcf6607817fb754836d87dd
Anna Poltieva <apolti@softserveinc.com>
2021-09-14 18:19:50 +0300

    Add after_commit hook for uodating counter_cache

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,12 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :event
+  after_commit :update_counter_in_event
+
+  private
+
+  def update_counter_in_event
+    event.update_column(:orders_count,
+                        Order.where(event_id: event_id).sum(:quantity))
+  end
 end
