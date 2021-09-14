dca226c5cdf564f06df456e6a553e6bc6ae4d201
Anna Poltieva <apolti@softserveinc.com>
2021-09-14 16:16:35 +0300

    Add validations to Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,8 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  validates_presence_of :event_id, :user_id, :uuid, :quantity
+  validates_numericality_of :quantity, greater_than_or_equal_to: 0, only_integer: true
+  validates_uniqueness_of :uuid
 end
