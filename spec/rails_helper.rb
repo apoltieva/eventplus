b28f68074a576a89943bd5ccfe585c38f67b27b1
Anna Poltieva <apolti@softserveinc.com>
2021-09-18 16:41:03 +0300

    Resolve conflicts

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,6 +29,8 @@ require 'rspec/rails'
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
+#
+require_relative './support/sign_in_support'
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
@@ -39,6 +41,7 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include EventHelper, type: :system
+  config.include ValidUserRequestHelper, type: :request
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
