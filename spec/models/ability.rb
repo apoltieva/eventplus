b875d9c92af2c1174b1f4259acace9b5d3d4ad48
Anna Poltieva <apolti@softserveinc.com>
2021-09-21 11:08:44 +0300

    Add missing permisssions validations

--- spec/models/ability.rb
+++ spec/models/ability.rb
@@ -16,12 +16,16 @@ RSpec.describe Ability, type: :model do
           it { is_expected.to be_able_to(:access, Venue) }
           it { is_expected.to be_able_to(:create, Venue) }
           it { is_expected.to be_able_to(:read, Venue) }
+          it { is_expected.to be_able_to(:new, Venue) }
+          it { is_expected.to be_able_to(:edit, Venue) }
           it { is_expected.to be_able_to(:update, Venue) }
           it { is_expected.to be_able_to(:delete, Venue) }
         end
         context 'can manage Event' do
           it { is_expected.to be_able_to(:access, Event) }
           it { is_expected.to be_able_to(:create, Event) }
+          it { is_expected.to be_able_to(:edit, Event) }
+          it { is_expected.to be_able_to(:new, Event) }
           it { is_expected.to be_able_to(:read, Event) }
           it { is_expected.to be_able_to(:update, Event) }
           it { is_expected.to be_able_to(:delete, Event) }
@@ -37,10 +41,14 @@ RSpec.describe Ability, type: :model do
         it { is_expected.to be_able_to(:create, Order) }
 
         it { is_expected.to_not be_able_to(:update, Event) }
+        it { is_expected.to_not be_able_to(:edit, Event) }
+        it { is_expected.to_not be_able_to(:new, Event) }
         it { is_expected.to_not be_able_to(:delete, Event) }
         it { is_expected.to_not be_able_to(:access, Venue) }
         it { is_expected.to_not be_able_to(:create, Venue) }
         it { is_expected.to_not be_able_to(:read, Venue) }
+        it { is_expected.to_not be_able_to(:new, Venue) }
+        it { is_expected.to_not be_able_to(:edit, Venue) }
         it { is_expected.to_not be_able_to(:update, Venue) }
         it { is_expected.to_not be_able_to(:delete, Venue) }
       end
