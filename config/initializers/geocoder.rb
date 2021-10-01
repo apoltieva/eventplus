dee16c693e9f17a243368dc71ff3ae8364ed046f
Anna Poltieva <apolti@softserveinc.com>
2021-10-01 18:19:57 +0300

    Test

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -3,7 +3,7 @@ Geocoder.configure(
     api_key: Rails.application.credentials.google_maps_api_key
   },
   # IP address geocoding service (default :ipinfo_io)
-  ip_lookup: :ipapi_com,
+  ip_lookup: :ipinfo_io,
   timeout: 10,
   units: :km
 )
