d5bd2f250cb7b0f01fd12baedcdb2431cf60a910
Anna Poltieva <apolti@softserveinc.com>
2021-10-01 17:16:24 +0300

    Add geocoder config

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,7 +6,7 @@ class EventsController < ApplicationController
   before_action :find_events_num_of_tickets, only: %i[index show]
 
   def index
-    @location = Geocoder.search(request.ip).first.address
+    @location = request.safe_location.address
     # if current_user
     #   user_id = current_user.id
     #   @order = Order.new
--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,3 +1,23 @@
 Geocoder.configure(
-  :timeout => 20
+  google: {
+    api_key: Rails.application.credentials.google_maps_api_key
+  },
+
+  # IP address geocoding service (default :ipinfo_io)
+  ip_lookup: :ipapi_com,
+
+  # to use an API key:
+  api_key: "...",
+
+  # geocoding service request timeout, in seconds (default 3):
+  timeout: 10,
+
+  # set default units to kilometers:
+  units: :km,
+
+  # caching (see Caching section below for details):
+  cache: Redis.new,
+  cache_prefix: "..."
+
 )
+
