de8b779a52dd14159313400dbaf4f0b0de348583
Anna Poltieva <apolti@softserveinc.com>
2021-10-04 17:11:28 +0300

    Fix rubocop

--- spec/support/event_helper.rb
+++ spec/support/event_helper.rb
@@ -9,21 +9,21 @@ module EventHelper
     expect(page).to have_text('Tickets left')
   end
 
-  def visit_first_event(admin, e)
+  def visit_first_event(admin, event)
     if admin
       user1 = create(:user)
       user2 = create(:user)
-      order1 = create(:order, event_id: e.id, user_id: user1.id)
-      order2 = create(:order, event_id: e.id, user_id: user2.id)
+      order1 = create(:order, event_id: event.id, user_id: user1.id)
+      order2 = create(:order, event_id: event.id, user_id: user2.id)
     end
-    click_link e.title
-    expect(page).to have_current_path event_path(e.id)
-    check_event_info(e)
-    if admin
-      expect(page).to have_text(user1.email)
-      expect(page).to have_text(user2.email)
-      expect(page).to have_text(order1.quantity)
-      expect(page).to have_text(order2.quantity)
-    end
+    click_link event.title
+    expect(page).to have_current_path event_path(event.id)
+    check_event_info(event)
+    return unless admin
+
+    expect(page).to have_text(user1.email)
+    expect(page).to have_text(user2.email)
+    expect(page).to have_text(order1.quantity)
+    expect(page).to have_text(order2.quantity)
   end
 end
