bc214b62c1df3bbafd0acf885b03a7945aa80cba
Anna Poltieva <apolti@softserveinc.com>
2021-10-06 17:40:50 +0300

    Finish stripe integration

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,43 +1,17 @@
 # frozen_string_literal: true
 
 class OrdersController < ApplicationController
-
-  def new
-    @order = current_user.orders.build(order_params)
-  end
-
   def create
     @order = current_user.orders.build(order_params)
-    p '-------------------'
-    p @order
-    p '-------------------'
-    session = Stripe::Checkout::Session
-              .create({
-                        payment_method_types: ['card'],
-                        line_items: [{
-                          price_data: {
-                            currency: 'usd',
-                            product_data: {
-                              name: @order.event.title
-                            },
-                            unit_amount: @order.event.ticket_price,
-                          },
-                          quantity: @order.quantity
-                        }],
-                        mode: 'payment',
-                        success_url: 'https://example.com/success',
-                        cancel_url: 'https://example.com/cancel'
-                      })
-
-    redirect_to session.url
-    # @order = current_user.orders.build(order_params)
-    # if @order.save
-    #   flash[:notice] = 'Your tickets will be sent to your email. Thanks for your purchase!'
-    #   TicketSender.send_tickets_for @order
-    # else
-    #   flash[:alert] = @order.errors.full_messages.join('; ')
-    # end
-    # redirect_back(fallback_location: root_path)
+    if @order.save
+      session = StripeCheckout.create_session(@order)
+      redirect_to session.url
+      # flash[:notice] = 'Your tickets will be sent to your email. Thanks for your purchase!'
+      #
+    else
+      flash[:alert] = @order.errors.full_messages.join('; ')
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def show
