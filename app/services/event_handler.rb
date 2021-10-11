66a746c0ec1c0232423e5388ae509ba0a06f9020
Anna Poltieva <apolti@softserveinc.com>
2021-10-11 13:05:00 +0300

    Fix event handler

--- app/services/event_handler.rb
+++ app/services/event_handler.rb
@@ -8,17 +8,17 @@ class EventHandler
       session = event.data.object
       customer, order = find_customer_and_order(session)
       if session.payment_status == 'paid'
-        set_status_and_stripe_id(:success, session.id)
+        set_status_and_stripe_id(order, :success, session.id)
         TicketSender.send_tickets_for order
       else
-        set_status_and_stripe_id(:failure, session.id)
+        set_status_and_stripe_id(order, :failure, session.id)
         FailureMailer.with(customer: customer)
                      .inform_about_checkout_failure.deliver_later
       end
     when 'charge.failed'
       session = event.data.object
       customer, order = find_customer_and_order(session)
-      set_status_and_stripe_id(:failure, session.id)
+      set_status_and_stripe_id(order, :failure, session.id)
       logger.log("Charge failed for order: #{order.id} of customer: #{customer.id}")
     else
       raise Exceptions::InvalidEventType
@@ -30,7 +30,7 @@ class EventHandler
     [customer, customer.orders.where(status: :created).last]
   end
 
-  def self.set_status_and_stripe_id(status, id)
+  def self.set_status_and_stripe_id(order, status, id)
     order.status = status
     order.stripe_id = id
     order.save
