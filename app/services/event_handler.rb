d9d2156cfa1c06ca16987f54c643b9b80b88d683
Anna Poltieva <apolti@softserveinc.com>
2021-10-11 10:13:33 +0300

    Refactor event_handler.rb

--- app/services/event_handler.rb
+++ app/services/event_handler.rb
@@ -2,26 +2,24 @@ class EventHandler
   def self.handle(event)
     if event.type == 'checkout.session.completed'
       session = event.data.object
-      find_customer_and_order(session)
+      customer, order = find_customer_and_order(session)
       if session.payment_status == 'paid'
-        @order.status = :success
-        TicketSender.send_tickets_for @order
+        order.status = :success
+        TicketSender.send_tickets_for order
       else
-        @order.status = :failure
-        FailureMailer.with(customer: @customer)
+        order.status = :failure
+        FailureMailer.with(customer: customer)
                      .inform_about_checkout_failure.deliver_later
       end
-      @order.stripe_id = session.id
-      @order.save
+      order.stripe_id = session.id
+      order.save
     else
       render json: { error: "Unhandled event type: #{event.type}" }, status: :bad_request
     end
   end
 
-  private
-
-  def find_customer_and_order(session)
-    @customer = Customer.find_by(stripe_id: session.customer)
-    @order = @customer.orders.first
+  def self.find_customer_and_order(session)
+    customer = Customer.find_by(stripe_id: session.customer)
+    [customer, customer.orders.first]
   end
 end
