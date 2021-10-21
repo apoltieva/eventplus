eb5884a8d72d6633a6ff617e5105ba12b1d89119
Anna Poltieva <apolti@softserveinc.com>
2021-10-21 14:21:06 +0300

    Fix string break

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,8 +24,8 @@ RSpec.configure do |config|
 
   config.before(:each) do
     Capybara.page.driver.header(
-      'user-agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko)' /
-      ' Chrome/41.0.2228.0 Safari/537.36.'
+      'user-agent',
+      'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 Chrome/41.0.2228.0 Safari/537.36.'
     )
   end
 
