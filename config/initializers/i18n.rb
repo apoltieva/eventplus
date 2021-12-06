0953659e4b2658ece84d5c3fe4372af1dd5c2095
Anna Poltieva <apolti@softserveinc.com>
2021-12-06 18:51:26 +0200

    Add English localozation

---
+++ config/initializers/i18n.rb
@@ -0,0 +1,8 @@
+I18n.default_locale = :en
+
+I18n.available_locales = [:en, :ua]
+
+LANGUAGES = [
+  %w[English en],
+  %w[Ukrainian ua]
+]
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :events
-  resources :venues, except: [:show]
-  root to: 'events#index'
-  devise_for :users, controllers: {
-    sign_up: 'users/sign_up'
-  }
+  scope '(:locale)' do
+    resources :events
+    resources :venues, except: [:show]
+    root to: 'events#index'
+    devise_for :users, controllers: {
+      sign_up: 'users/sign_up'
+    }
+
+    get '/success', to: 'orders#success', as: :success_checkout
+    get '/cancel', to: 'orders#cancel', as: :cancel_checkout
+    get '/help', to: 'events#help', as: :help
+  end
+
   resources :orders, only: %i[create show new]
   resources :webhooks, only: %i[create]
-  get '/success', to: 'orders#success', as: :success_checkout
-  get '/cancel', to: 'orders#cancel', as: :cancel_checkout
-  get '/help', to: 'events#help', as: :help
 
   mount ActionCable.server, at: '/cable'
 end
